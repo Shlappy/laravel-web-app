6446e569ca8a4929b27081320a8c68ab3829f323
Shlappy <dudu_man@mail.ru>
2022-06-30 17:34:52 +1000

    save changes

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -2,7 +2,7 @@
   <div class="container container--lg">
     <div class="product__blocks">
 
-      <x-products.filters id="filters" :filters="$filters" class="filter filter__products"></x-products.filters>
+      <x-products.filters x-init="$store.products.storeFilterElements()" id="filters" :filters="$filters" class="filter filter__products"></x-products.filters>
 
       <div class="product__catalog-wrapper" x-data x-html="$store.products.html">
       </div>
@@ -10,23 +10,44 @@
     </div>
   </div>
   <script>
+    const optionChange = new Event('optionChange', {
+      bubbles: true
+    });
+
     document.addEventListener('alpine:init', () => {
       Alpine.store('products', {
         html: <?= str_replace(
-          ['\u0022', '\u0027'], ["\\\"", "\\'"], 
-          json_encode(view('components.product.product-list', compact('products'))->render())
-        ) ?>,
-        // filterElements: [],
- 
+                ['\u0022', '\u0027'],
+                ["\\\"", "\\'"],
+                json_encode(view('components.product.product-list', compact('products'))->render())
+              ) ?>,
+        filterElements: [],
+        affectedFilters: [],
+
         async get(url = window.location.href) {
           await axios.get(url).then(response => this.html = response.data);
-          window.scrollTo({top: 0, behavior: 'smooth'});
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
         },
 
-        // storeFilterElement(el) {
-        //   this.filterElements.push(el);
-        //   console.log(this.filterElements)
-        // }
+        storeFilterElements(el) {
+          this.filterElements = document.querySelectorAll('#filters .filter-item');
+          let checkboxes = document.querySelectorAll('#filters .filter-item .form-input__checkbox input');
+
+          this.filterElements.forEach(filterEl => {
+            filterEl.addEventListener('optionChange', function(e) {
+              console.log(`event triggered by: ${e}`)
+              this.affectedFilters.push(filterEl)
+              // if (!this.affectedFilters.includes(filterEl)) {this.affectedFilters.push(filterEl); console.log(filterEl);} else {console.log('in there')};
+            })
+          })
+
+          checkboxes.forEach(el => {
+            el.addEventListener('change', e => e.target.dispatchEvent(optionChange))
+          });
+        }
       });
     });
   </script>
